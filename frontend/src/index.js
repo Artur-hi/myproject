9738e11250d6acd772bec52031f08dfa3e881aeb
Artur-hi <artur.hi_rul@mail.ru>
2021-08-14 18:24:42 +0300

    Initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import N2O from 'n2o-framework';
+
+import 'n2o-framework/dist/n2o.css';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <N2O />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
